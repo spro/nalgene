e78efe69ed0de4bc1780ab3ad3bedba70f600eff
Sean Robertson <sprobertson@gmail.com>
2017-02-20 20:55:31 -0800

    reworking parsing and generation with better separation of concerns; context tree to pre-choose branches

--- generate.py
+++ generate.py
@@ -1,79 +1,88 @@
-import re
-import random
-import sys
+from nalgene.parse import *
 import os
+import json
 
-def parseSections(filename, base_dir='.'):
-    sections = open(os.path.join(base_dir, filename)).read().strip().split('\n\n')
-    sections = [section.split('\n') for section in sections]
-    all_sections = []
-    if sections[0][0].startswith('@'):
-        import_filenames = [line.split(' ')[1] for line in sections[0]]
-        for import_filename in import_filenames:
-            all_sections += parseSections(import_filename, base_dir)
-    all_sections += [parseSection(section) for section in sections]
-    return all_sections
+# Generate tokens up to $value level
 
-def parseSection(section):
-    key = section.pop(0)
-    options = {'passthrough': False}
-    if key.endswith('='):
-        options['passthrough'] = True
-        key = re.sub(r'=$', '', key)
-    lines = [re.sub(r'^    ', '', line) for line in section]
-    lines = [line.split(' ') for line in lines if not line.startswith('#')]
-    return (key, {'lines': lines, 'options': options})
+def walk_tree(root, current, context, start_w=0):
+    # print('[walk_tree]', '"' + current.key + '"')
 
-def generate(generate_token='%', indent=0):
-    ind = ' ' * indent * 4
-    section = sections[generate_token]
-    tokens = random.choice(section['lines'])
-    output = ''
-    output_ = []
+    seq = random.choice(current)
+    flat = Node('>')
+    tree = Node(current.key)
+    for child in seq:
+        child_key = child.key
 
-    for token in tokens:
-        if token.endswith('?'):
-            token = re.sub(r'\?$', '', token)
+        # Optionally skip optional tokens
+        if child_key.endswith('?'):
+            child_key = child_key[:-1]
             if random.random() < 0.5:
                 continue
-        if token.startswith(('%', '$', '~')):
-            token_output, token_output_ = generate(token, indent+1)
-            output += token_output + ' '
-            if sections[token]['options']['passthrough']:
-                output_ += token_output_
-            elif token.startswith('%'):
-                output_ += [[token] + token_output_]
-            elif token.startswith('$'):
-                output_ += [[token, token_output]]
+
+        # Expandable word, e.g. %phrase or ~synonym
+        if child_key.startswith(('%', '~', '$')):
+
+            # Existing value, pass in context
+            if (context != None) and (child_key in context):
+                sub_context = context[child_key]
+
+            else:
+                sub_context = None
+
+            sub_flat, sub_tree = walk_tree(root, root[child_key], sub_context, start_w)
+
+            flat.merge(sub_flat)
+
+            # Adjust position based on number of tokens
+            len_w = len(sub_flat)
+            sub_tree.position = (start_w, start_w + len_w - 1, len_w)
+            start_w += len_w
+
+            if not child_key.startswith('~'):
+                if root[child_key].passthrough:
+                    tree.merge(sub_tree)
+                else:
+                    tree.add(sub_tree)
+
+        # Terminal node, e.g. a word
         else:
-            output += token + ' '
+            start_w += 1
+            len_w = 1
+            flat.add(child_key)
 
-    output = output.strip()
-    if generate_token == '%':
-        output_ = ['%'] + output_
+    return flat, tree
 
-    return output, output_
+def generate_from_file(base_dir, filename):
+    root_context = Node('')
+    parsed = parse_file(base_dir, filename)
+    parsed.map_leaves(tokenizeLeaf)
+    walked_flat, walked_tree = walk_tree(parsed, parsed['%'], root_context['%'])
+    print(walked_flat)
+    print('>', walked_flat.raw_str)
+    print('= tree', walked_tree)
+    print('')
+    return walked_flat, walked_tree
 
-def w(s): sys.stdout.write(s)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python generate.py [grammar].nlg")
+        sys.exit()
 
-def lprint(a, n_indent=0):
-    indent = ' ' * n_indent * 4
-    w(indent + '(')
-    for i in a:
-        if isinstance(i, list):
-            w('\n')
-            lprint(i, n_indent + 1)
-        else:
-            w(' ' + i)
-    w(' )')
+    filename = os.path.realpath(sys.argv[1])
+    base_dir = os.path.dirname(filename)
+    filename = os.path.basename(filename)
 
-if len(sys.argv) < 2:
-    print("Usage: python generate.py [grammar].nlg")
-    sys.exit()
-filename = os.path.basename(sys.argv[1])
-base_dir = os.path.dirname(sys.argv[1])
-sections = dict(parseSections(filename, base_dir))
-generated_output, generated_output_ = generate()
-print('>', generated_output)
-lprint(generated_output_)
-print()
+# test_json = json.load(open(base_dir + '/test2.json'))
+# root_context = Node('').add(parse_dict(test_json))
+    root_context = Node('')
+
+    generate_from_file(base_dir, filename)
+
+else:
+    filename = sys.argv[1]
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    combined = os.path.join(base_dir, filename)
+    base_dir = os.path.dirname(combined)
+    filename = os.path.basename(combined)
+
+    def generate(): return generate_from_file(base_dir, filename)
--- parse.py
+++ parse.py
@@ -1,248 +1,73 @@
 import re
 import math
 import random
+import json
+import sys
+from nalgene.node import *
 
-print('-' * 80)
-file = open('nests.nlg').read()
 SHIFT_WIDTH = 4
 
 start_space = r'^(    )*'
-
-types = {
-    '%': 'phrase',
-    '#': 'hash',
-    '~': 'synonym',
-    '$': 'value',
-    '@': 'function'
-}
-
-class Node:
-    def __init__(self, key, type='*'):
-        self.key = key
-        self.children = []
-        self.children_by_key = {}
-        self.is_leaf = True
-        self.type = type
-
-    @property
-    def value(self):
-        return self.children[0].key
-
-    def __str__(self):
-        return self.str(0)
-
-    def __getitem__(self, key):
-        return self.children_by_key[key]
-
-    def __contains__(self, key):
-        return self.children_by_key.__contains__(key[0])
-
-    def str(self, indent=0):
-        s = ""
-        s += (' ' * indent * 4) + self.type + ' ' + str(self.key)
-        for child in self.children:
-            s += "\n" + child.str(indent + 1)
-        return s
-
-    def add(self, child):
-        if not isinstance(child, Node):
-            child = Node(child)
-        self.children.append(child)
-        self.children_by_key[child.key] = child
-        self.is_leaf = False
-        return child
-
-    def merge(self, child):
-        if not isinstance(child, Node):
-            child = Node(child)
-        for child_child in child.children:
-            self.children.append(child_child)
-            self.children_by_key[child_child.key] = child_child
-            self.is_leaf = False
-        return self
-
-    def descend(self, i_s):
-        child = self.children[i_s[0]]
-        if len(i_s) == 1:
-            return child
-        else:
-            return child.descend(i_s[1:])
-
-    def add_at(self, child, i_s):
-        if len(i_s) == 1:
-            self.add(child)
-        else:
-            self.children[i_s[0]].add_at(child, i_s[1:])
-
-    @property
-    def is_array(self):
-        if len(self.children) == 0:
-            return False
-        for child in self.children:
-            if not child.is_leaf:
-                return False
-        return True
-
-    def __iter__(self):
-        self.iter_c = 0
-        return self
-
-    def __next__(self):
-        if self.iter_c >= len(self.children):
-            raise StopIteration
-        else:
-            child = self.children[self.iter_c]
-            self.iter_c += 1
-            return child
-
-    def mapLeaves(self, f):
-        for child in self.children:
-            if child.is_leaf:
-                f(child)
-            else:
-                child.mapLeaves(f)
 
 def count_indent(s):
     indent = len(re.match(start_space, s).group(0))
     return math.floor(indent / SHIFT_WIDTH)
 
-def parse_section(section):
-    lines = section.split('\n')
+def parse_string(base_dir, string):
+    lines = string.split('\n')
     lines = [line for line in lines if not re.match(r'^\s*#', line)]
     parsed = Node('')
-    i_s = [-1]
+    indexes = [-1]
     level = 0
     last_ind = 0
+
     for li in range(len(lines)):
         line = lines[li]
         ind = count_indent(line)
         line = re.sub(start_space, '', line).strip()
         if len(line) == 0: continue
 
+        if level == 0 and line.startswith('@import'):
+            filename = line.split(' ')[1]
+            imported = parse_file(base_dir, filename)
+            for child in imported:
+                parsed.add(child)
+                indexes[level] += 1
+            continue
+
         if ind == last_ind: # Next item in a list
-            i_s[level] += 1
+            indexes[level] += 1
         elif ind > last_ind: # Child item
             level += 1
-            i_s.append(0)
+            indexes.append(0)
         elif ind < last_ind: # Up to next item in parent list
             diff = (last_ind - ind)
             for i in range(last_ind - ind):
                 level -= 1
-                i_s.pop()
-            i_s[level] += 1
+                indexes.pop()
+            indexes[level] += 1
 
-        parsed.add_at(line, i_s)
+        parsed.add_at(line, indexes)
         last_ind = ind
+
     return parsed
 
-parsed = parse_section(file)
-
-# Iterate over generic parsed tree to create specific node types
-for p in parsed:
-    p.type = types[p.key[0]]
-    if p.key.endswith('='):
-        p.passthrough = True
-        p.key = p.key[:-1]
-
 def tokenizeLeaf(n):
-    n.type = 'sequence'
-    n.sequence = n.key.split(' ')
+    n.type = 'seq'
+    for s in n.key.split(' '):
+        added = n.add(s)
+    n.key = 'seq'
 
-parsed.mapLeaves(tokenizeLeaf)
+def parse_file(base_dir, filename):
+    parsed = parse_string(base_dir, open(base_dir + '/' + filename).read())
+    return parsed
 
-def generate(from_node, context={}):
-    print('[generate]', from_node)
-    sentence = ""
-    tree = Node(from_node.key)
-    sequence = random.choice(from_node.children)
-
-    for token in sequence.sequence:
-        print('[generate] %s' % from_node.key, token)
-
-        # Optional
-        if token.endswith('?'):
-            if random.random() < 0.5:
-                continue
-            else:
-                token = token[:-1]
-
-        # Hash
-        if token.startswith('#'):
-            hash_name = token.split('[')[0]
-            hash_ref = token.split('[')[1].split(']')[0]
-
-            # Get the object referenced
-            token_parts = hash_ref.split('.')
-            token = token_parts[0]
-            sub_token = token_parts[1]
-            obj = context[token]
-            hash_key = obj[sub_token].value
-
-            # Get the hash and value from it
-            hash = parsed[hash_name]
-            tree.add(hash[hash_key].value)
-
-        # Value
-        elif token.startswith(('$', '%')):
-            token_parts = token.split('.')
-
-            # Object value
-            if len(token_parts) == 2:
-                super_token = token_parts[0]
-                sub_token = token_parts[1]
-
-                # Check if super-object exists
-                if super_token in context:
-                    obj = context[super_token]
-
-                # Or choose one
-                else:
-                    type = parsed[super_token]
-                    obj = random.choice(type.children)
-                    context[super_token] = obj
-
-                sentence += obj[sub_token].value
-                tree.add(obj[sub_token].value)
-
-            # Regular value
-            else:
-                sub_sentence, sub_tree = generate(parsed[token], context)
-                sentence += sub_sentence
-                tree.add(sub_tree)
-
-        # Syonym
-        elif token.startswith('~'):
-            sub_sentence, sub_tree = generate(parsed[token], context)
-            sentence += sub_sentence
-            tree.merge(sub_tree)
-
-        # Other
-        else:
-            sentence += token + ' '
-            if from_node.type == 'value':
-                tree.add(token)
-
-    return sentence, tree
-
-sentence, tree = generate(parsed['%'])
-print('>', sentence)
-print(tree)
-
-def extract(generated):
-    sentence = ""
-    tree = Node(generated.key)
-    for node in generated:
-        # Add words to sentence
-        if node.is_leaf:
-            sentence += node.key + ' '
-            # Keep variable values
-            if generated.key[0] == '$':
-                tree.add(node.key)
-        # Add other nodes to tree
-        else:
-            sub_sentence, sub_tree = extract(node)
-            sentence += sub_sentence
-            tree.add(sub_tree)
-    return sentence, tree
-
-
+def parse_dict(obj, obj_key='%'):
+    tree = Node(obj_key)
+    if isinstance(obj, dict):
+        for key in obj.keys():
+            tree.add(parse_dict(obj[key], key))
+        return tree
+    else:
+        tree.add(obj)
+        return tree
